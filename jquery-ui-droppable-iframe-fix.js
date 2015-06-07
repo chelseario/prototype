2fd61312f7cf60d4bfa038b9095b46b780b96788
Kevin H. Kamel <kamelkev@underprint.com>
2015-06-07 17:22:59 -0600

    Update jquery-ui-droppable-iframe-fix.js

--- jquery-ui-droppable-iframe-fix.js
+++ jquery-ui-droppable-iframe-fix.js
@@ -19,7 +19,7 @@ $.ui.intersect = (function() {
 			r = l + droppable.proportions().width,
 			b = t + droppable.proportions().height;
    
-        alert('over here');
+  
     	if (1)
     	{
             
@@ -28,7 +28,6 @@ $.ui.intersect = (function() {
       		iframeHeight = $(droppable.iframe).height(),
       		iframeScrollTop = $(droppable.iframe).contents().scrollTop(),
       		iframeScrollLeft = $(droppable.iframe).contents().scrollLeft();
-            alert(iframeOffset);
         
 		    if (y1 < iframeOffset.top || x1 < iframeOffset.left || x1 + draggable.helperProportions.width > iframeOffset.left + iframeWidth || y1 + draggable.helperProportions.height > iframeOffset.top + iframeHeight) // outside iframe;
       	{
